5d748b22bb52194216787ca9af6a6f11c5cb35bc
Syed Ali Ahmed <syedaliahmad22@gmail.com>
2024-04-30 11:05:59 +0500

    first commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "mern-enclave.firebaseapp.com",
+  projectId: "mern-enclave",
+  storageBucket: "mern-enclave.appspot.com",
+  messagingSenderId: "91125893134",
+  appId: "1:91125893134:web:1a17b328a5db0d08e3945f",
+  measurementId: "G-HK74K2PNGD"
 };
 
 // Initialize Firebase
