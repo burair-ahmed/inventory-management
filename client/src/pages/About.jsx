6dbf846351a6b80e32d5bd5fa3a75ebca8d64afa
Syed Ali Ahmed <syedaliahmad22@gmail.com>
2024-04-30 17:29:41 +0500

    $ removal

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,9 +4,9 @@ export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Indus Enclave</h1>
-      <p className='mb-4 text-slate-700'>Indus Enclave is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
+      <p className='mb-4 text-slate-700'>A Premier low cost housing scheme along with amazing occupational opportunities,houses and property for sale Located at a premium location of Nooriabad on main Karachi - Hyderabad M9 Motorway and equiped with all modern day facilities in easy monthly installment</p>
       <p className='mb-4 text-slate-700'>
-      Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+      Indus enclave is a boundary walled housing project designed to deiliver luxurious quality of life at very reasonable and affordable prices.
       </p>
       <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
     </div>
